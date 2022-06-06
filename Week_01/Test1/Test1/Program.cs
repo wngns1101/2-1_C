5857030d7cfe3f9c3adbd279a627cb5bd40d5b15
이주훈 <86885227+wngns1101@users.noreply.github.com>
2022-06-06 14:34:36 +0900

    1주차

---
+++ Week_01/Test1/Test1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTime
+{
+    internal class FirstClass
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 2 % 3;
+            int c = a / b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
